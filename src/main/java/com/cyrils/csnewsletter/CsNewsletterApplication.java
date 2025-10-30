fe6515b8252d3843109f9fb8115b9d6085d38f35
cyrils <73370044+nightguarder@users.noreply.github.com>
2025-10-30 12:32:01 +0100

    Camunda 8 Springboot Initializr

---
+++ src/main/java/com/cyrils/csnewsletter/CsNewsletterApplication.java
@@ -0,0 +1,13 @@
+package com.cyrils.csnewsletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsNewsletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsNewsletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyrils/csnewsletter/CsNewsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyrils.csnewsletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsNewsletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
